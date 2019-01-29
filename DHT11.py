6b1477dc24aef397aec8afedbd5cfa1d99c058a2
Lazar Milenkovic <milenkovic.lazar@gmail.com>
2019-01-29 18:37:49 +0100

    ready for Pi

--- DHT11.py
+++ DHT11.py
@@ -9,11 +9,9 @@ import datetime
 sensor = Adafruit_DHT.DHT11
 pin = '4'
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 now = datetime.datetime.now()
 
 if humidity is not None and temperature is not None:
-    print('{0:0.1f}* {1:0.1f}% %s %s %s'.format(temperature, humidity, now.hour, now.minute))
+    print('{:0.1f}* {:0.1f}% {:d} {:d}'.format(temperature, humidity, now.hour, now.minute))
